680937f0c515ea6f1a68998633b6ce5f48dc291f
CoderYounes <younesanas2021@gmail.com>
2023-12-23 00:02:49 +0100

    add the path

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,8 @@
+# Create File in certain 'path' with 'content'
 file { '/tmp/school':
   ensure  => file,
   mode    => '0744',
+  path    => '/tmp/school'
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet',
