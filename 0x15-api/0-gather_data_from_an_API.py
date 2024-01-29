e1fc4facd38317fbb18fdda26f941359852d7b91
CoderYounes <younesanas2021@gmail.com>
2024-01-29 20:40:39 +0100

    let try the first one

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,32 @@
 import requests
 import sys
 
-# Get request To This URL : https://jsonplaceholder.typicode.com/todos
-# Parse the Json Reponse to Extract the Employer name from https://jsonplaceholder.typicode.com/users
-# take the ID as argument
-#  Print them in Certain Format (Name & Done_Tasks & Total Number of Tasks)
-# Loop & print eh entire Completed Task Use 1 Tabulation & 1 Space
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         sys.exit(1)
     else:
-        User_ID = sys.argv[1]
+        uID = sys.argv[1]
         ToDo_URL = "https://jsonplaceholder.typicode.com/todos"
-
+        User_URL = "https://jsonplaceholder.typicode.com/users/{}".format(uID)
 
         Todo_r = requests.get(ToDo_URL)
+        User = requests.get(User_URL)
+        reponse = User.json()
         Todo_j = Todo_r.json()
+        count = 0
+        completed = 0
         for todo in Todo_j:
-            title = todo.get('title')
-            print("{}".format(title))
+            if todo.get('userId') == int(uID):
+                title = todo.get('title')
+                count += 1
+                if todo.get('completed') is True:
+                    completed += 1
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            reponse.get('name'), completed, count
+        ))
+        for task in Todo_j:
+            if task.get('userId') == int(uID):
+                if task.get('completed') is True:
+                    print(f"\t {task.get('title')}")
