5924b1c8db6272c508f442f24bf169430db2be4c
Coderyounes <younesanas2021@gmail.com>
2024-02-13 18:12:06 +0100

    feat(new function) : return the number of subscribers for a subbredit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Python Script return the number of subscribers """
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+        number_of_subscribers Function Scrap Subscribers Number
+            subrredit - channel to check subscribers number
+            return - subscribers number
+            return - 0 if subreddit not found
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    header = {'User-agent': "linux.ALX-Tasks:v1.0.0 (by /u/Dizzy_Back7390)"}
+
+    response = requests.get(url, headers=header)
+    if response.status_code != 404:
+        r_json = response.json()
+        data = r_json.get('data')
+        subs = data.get('subscribers')
+        return subs
+    return 0
