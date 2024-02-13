65fcfe56c558956c1c7147061dd850c20a360ecc
Coderyounes <younesanas2021@gmail.com>
2024-02-13 19:53:25 +0100

    Task 1: return the hot topics titles

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+""" DOC DOC DOC"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """
+        top_ten - function return hot post titles
+        return - the title or None if fail
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "linux:uhXwbQac5CHtsASxHSxtjQ:1.0.0\
+            (by /u/Dizzy_Back7390)"}
+
+    response = get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
+
+    else:
+        print("None")
