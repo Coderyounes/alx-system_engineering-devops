68334fef424b45e6fb9b060b0e494d752019cba2
Coderyounes <younesanas2021@gmail.com>
2024-02-14 21:15:32 +0100

    change the user-agent again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -15,8 +15,8 @@ def number_of_subscribers(subreddit):
     session = requests.Session()
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        "User-Agent": "User-Agent: MyAwesomeApp/1.0\
-        (Linux; U; en-US; your_username/Reddit)",
+        "User-Agent": "User-Agent: ALX-Tasks/1.0\
+        (Linux; U; en-US; Dizzy_Back7390/Reddit)",
         }
     session.headers.update(headers)
     response = session.get(url, allow_redirects=False)
