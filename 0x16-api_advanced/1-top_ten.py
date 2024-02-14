37dcd59283ace92c14573d99bf429fce26c711d5
Coderyounes <younesanas2021@gmail.com>
2024-02-14 22:24:43 +0100

    new update

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,14 +10,15 @@ def top_ten(subreddit):
         return - the title or None if fail
     """
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {"User-Agent": "linux:uhXwbQac5CHtsASxHSxtjQ:1.0.0\
+    headers = {"User-Agent": "python:uhXwbQac5CHtsASxHSxtjQ:1.0.0\
             (by /u/Dizzy_Back7390)"}
-
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
