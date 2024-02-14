4760614578098de542f7e4627384436e7d2a64cd
Coderyounes <younesanas2021@gmail.com>
2024-02-14 22:42:21 +0100

    new test again

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """ Python Script return the number of subscribers """
-
-
 import requests
+
+
+headers = {'user-agent': 'my-app/0.0.1'}
 
 
 def number_of_subscribers(subreddit):
@@ -13,7 +14,6 @@ def number_of_subscribers(subreddit):
             return - 0 if subreddit not found
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'chalwe_demo_reddit_module'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if not response.status_code == 200:
