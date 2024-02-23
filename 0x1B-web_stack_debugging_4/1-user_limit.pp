ba5088f4da84e14ac1a5e041f98d6194466e0a81
Coderyounes <younesanas2021@gmail.com>
2024-02-23 03:24:06 +0100

    try to increase the maxlogin

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,4 @@
-# Replace Holberton with foo
-exec { 'update_limits_conf':
-  command     => '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
+# A puppet manifest to change user limit
+exec { 'increase-user-limit' :
+  command => "/bin/sed -i 's/4/40/g' /etc/security/limits.conf",
 }
