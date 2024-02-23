654ec36d97dfe3c99ab46d24e1a95792e67c5496
Coderyounes <younesanas2021@gmail.com>
2024-02-23 03:25:25 +0100

    try this Crap

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,2 @@
 # A puppet manifest to change user limit
-exec { 'increase-user-limit' :
-  command => "/bin/sed -i 's/4/40/g' /etc/security/limits.conf",
-}
+exec { '/usr/bin/env sed -i "s/holberton/foo/" /etc/security/limits.conf': }
